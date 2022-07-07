5d294cf0f00a5436b0f26dd4c2394f5fa2bb5fc5
chungjung <chungjung.d@gmail.com>
2022-07-07 11:28:43 +0900

    .

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+import { join } from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: 'http://localhost:4000',
+      package: 'auth',
+      protoPath: join(__dirname, './_proto/auth.proto'),
+    },
+  });
+  await app.listen()
 }
+
 bootstrap();
