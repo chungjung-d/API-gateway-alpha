0f865de1db17cfe60515b28e0dbd0df669bdc1a1
chungjung <chungjung.d@gmail.com>
2022-08-02 16:08:07 +0900

    Transaction Setting

--- auth/src/application/query-handler/reissue-access-jwt.handler.ts
+++ auth/src/application/query-handler/reissue-access-jwt.handler.ts
@@ -8,6 +8,7 @@ import {
   ReissueAccessJwtQuery,
   ReissueAccessJwtQueryReturn,
 } from '../query/reissue-access-jwt.query';
+import { DataSource, EntityManager } from 'typeorm';
 
 @Injectable()
 @QueryHandler(ReissueAccessJwtQuery)
@@ -18,28 +19,42 @@ export class ReissueAccessJwtHandler
   constructor(
     private readonly configService: ConfigService,
     private readonly userRepository: UserRepository,
+    private readonly dataSource: DataSource,
   ) {}
 
   async execute(
     query: ReissueAccessJwtQuery,
   ): Promise<ReissueAccessJwtQueryReturn> {
-    const { refreshToken } = query.reissueAccessJwtQueryDTO;
+    return this.transactionCommit(query);
+  }
 
-    const verify = jwt.verify(
-      refreshToken,
-      this.configService.get('JWT_REFRESH_SECRET'),
+  async transactionCommit(
+    query: ReissueAccessJwtQuery,
+  ): Promise<ReissueAccessJwtQueryReturn> {
+    return await this.dataSource.manager.transaction(
+      async (transactionalEntityManager: EntityManager) => {
+        const { refreshToken } = query.reissueAccessJwtQueryDTO;
+
+        const verify = jwt.verify(
+          refreshToken,
+          this.configService.get('JWT_REFRESH_SECRET'),
+        );
+
+        const accessGenerateJWTTokenDTO = {
+          secret: this.configService.get('JWT_ACCESS_SECRET'),
+          expireTime: this.configService.get('JWT_ACCESS_EXPIRE_TIME'),
+        };
+
+        const user = await this.userRepository.findById(
+          verify.sub,
+          transactionalEntityManager,
+        );
+        const accessToken = await user.createAccessJWTToken(
+          accessGenerateJWTTokenDTO,
+        );
+
+        return { accessToken: accessToken };
+      },
     );
-
-    const accessGenerateJWTTokenDTO = {
-      secret: this.configService.get('JWT_ACCESS_SECRET'),
-      expireTime: this.configService.get('JWT_ACCESS_EXPIRE_TIME'),
-    };
-
-    const user = await this.userRepository.findById(verify.sub);
-    const accessToken = await user.createAccessJWTToken(
-      accessGenerateJWTTokenDTO,
-    );
-
-    return { accessToken: accessToken };
   }
 }
--- auth/src/domain/interface/repostory.interface.ts
+++ auth/src/domain/interface/repostory.interface.ts
@@ -1,9 +1,22 @@
 import { UserClass } from './user.class';
+import { EntityManager } from 'typeorm';
 
 export interface UserRepositoryInterface {
-  createUser: (new_user : UserClass) => Promise<void>
-  updateUser: (user : UserClass) => Promise<void>
-  findByUUID : (userUUID : string) => Promise<UserClass | null>
-  findById : (userEmailId: string) => Promise<UserClass>
-  findAll : () => Promise<UserClass[]>;
+  createUser: (
+    new_user: UserClass,
+    transactionalEntityManager: EntityManager,
+  ) => Promise<void>;
+  updateUser: (
+    user: UserClass,
+    transactionalEntityManager: EntityManager,
+  ) => Promise<void>;
+  findByUUID: (
+    userUUID: string,
+    transactionalEntityManager: EntityManager,
+  ) => Promise<UserClass | null>;
+  findById: (
+    userEmailId: string,
+    transactionalEntityManager: EntityManager,
+  ) => Promise<UserClass>;
+  findAll: (transactionalEntityManager: EntityManager) => Promise<UserClass[]>;
 }
